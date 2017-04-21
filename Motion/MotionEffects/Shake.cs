0641e28ce0d45c15112810080a7e6b1401641d17
Watson Tungjunyatham <starrodkirby86@mail.fresnostate.edu>
2017-04-21 14:22:55 -0700

    Add more rotations

--- Motion/MotionEffects/Shake.cs
+++ Motion/MotionEffects/Shake.cs
@@ -29,6 +29,19 @@ namespace StorybrewScripts
                         sprite.Move(i, i + step, pointA, pointB);
                     }
                 }
+
+                // Goal for this one is to create that sort of camera-moving effect.
+                // Maybe do something with extremely subtle easings and light movements.
+                public static void ShakeSubtle(OsbSprite sprite, int startTime, int endTime, Vector2 variance, int points = 20)
+                {
+                    var step = (endTime - startTime) / (float)points;
+                    for(float i = startTime; i < endTime; i += step)
+                    {
+                        var pointB = (Vector2)sprite.PositionAt(startTime);
+                        var pointA = pointB + new Vector2((float)(StoryboardObjectGenerator.Current.Random(0, variance.X) - variance.X / 2), (float)(StoryboardObjectGenerator.Current.Random(0, variance.Y) - variance.Y / 2));
+                        sprite.Move((OsbEasing)StoryboardObjectGenerator.Current.Random(0, Enum.GetNames(typeof(OsbEasing)).Length), i, i + step, pointA, pointB);
+                    }
+                }
             }
         }
     }
--- Motion/UtilityMethods.cs
+++ Motion/UtilityMethods.cs
@@ -18,17 +18,30 @@ namespace StorybrewScripts
         {
             class MotionHelper
             {
-
-                public static Vector2 Rotate(Vector2 p, float t) => new Vector2((float)(p.X * Math.Cos(t) - p.Y * Math.Sin(t)), (float)(p.X * Math.Sin(t) + p.Y * Math.Cos(t)));
-
-                public static Vector2 Rotate(Vector2 p, float t, Vector2 origin)
+                public static Vector2 Rotate(Vector2 p, double t, Vector2 origin)
                 {
                     p -= origin;
                     var rotatedP = new Vector2((float)(p.X * Math.Cos(t) - p.Y * Math.Sin(t)), (float)(p.X * Math.Sin(t) + p.Y * Math.Cos(t)));
                     return rotatedP + origin;
                 }
 
+                public static Vector2 Rotate(Vector2 p, double t) => Rotate(p, t, Vector2.Zero);
 
+                public static Vector2 Mirror(Vector2 p) => Rotate(p, MathHelper.Pi);
+                
+                public static Vector2 MirrorToScreen(Vector2 p) => Rotate(p, MathHelper.Pi, new Vector2(320, 240));
+
+                public static Vector2 MirrorHorizontal(Vector2 p)
+                {
+                    var rotation = MirrorToScreen(p);
+                    return new Vector2(rotation.X, p.Y);
+                }
+
+                public static Vector2 MirrorVertical(Vector2 p)
+                {
+                    var rotation = MirrorToScreen(p);
+                    return new Vector2(p.X, rotation.Y);
+                }
 
                 public static Vector2 XRange { get { return new Vector2(-107, 747); } }
 
